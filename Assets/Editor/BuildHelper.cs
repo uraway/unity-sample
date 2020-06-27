75a52d0a4381cac186f0d5f346fa08978bedfcf3
Masato Urai <masato.uraway@gmail.com>
2020-06-27 15:19:51 +0800

    Fix indent

--- Assets/Editor/BuildHelper.cs
+++ Assets/Editor/BuildHelper.cs
@@ -5,25 +5,25 @@ using UnityEditor;
 
 public static class BuildHelper
 {
-	public static void StandaloneWindows64()
-	{
-		BuildPipeline.BuildPlayer(GetScenes(), getLocationPath(), BuildTarget.StandaloneWindows64, BuildOptions.None);
-	}
+  public static void StandaloneWindows64()
+  {
+    BuildPipeline.BuildPlayer(GetScenes(), getLocationPath(), BuildTarget.StandaloneWindows64, BuildOptions.None);
+  }
 
-	public static void StandaloneLinux64()
-	{
-		BuildPipeline.BuildPlayer(GetScenes(), getLocationPath(), BuildTarget.StandaloneLinuxUniversal, BuildOptions.None);
-	}
+  public static void StandaloneLinux64()
+  {
+    BuildPipeline.BuildPlayer(GetScenes(), getLocationPath(), BuildTarget.StandaloneLinuxUniversal, BuildOptions.None);
+  }
 
-	public static void StandaloneOSX()
-	{
-		BuildPipeline.BuildPlayer(GetScenes(), getLocationPath(), BuildTarget.StandaloneOSX, BuildOptions.None);
-	}
+  public static void StandaloneOSX()
+  {
+    BuildPipeline.BuildPlayer(GetScenes(), getLocationPath(), BuildTarget.StandaloneOSX, BuildOptions.None);
+  }
 
-	private static string[] GetScenes()
-	{
-		return EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray();
-	}
+  private static string[] GetScenes()
+  {
+    return EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray();
+  }
 
     private static string getLocationPath()
     {
